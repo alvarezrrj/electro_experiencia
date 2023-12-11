cc1bf73c7b5d39804b081c1b1e41348735eeeb90
Rodrigo Alvarez <alvarezrrj@gmail.com>
2023-12-11 15:16:50 -0300

    Fix migration

--- usuario/prisma/migrations/20231211175506_change_cuit_data_type_to_int/migration.sql
+++ usuario/prisma/migrations/20231211175506_change_cuit_data_type_to_int/migration.sql
@@ -4,8 +4,6 @@
   - You are about to alter the column `cuit` on the `usuarios` table. The data in that column could be lost. The data in that column will be cast from `VarChar(32)` to `Int`.
 
 */
--- DropIndex
-DROP INDEX IF EXISTS `usuarios_cuit_key` ON `usuarios`;
 
 -- AlterTable
 ALTER TABLE `usuarios` MODIFY `cuit` INTEGER NOT NULL;
